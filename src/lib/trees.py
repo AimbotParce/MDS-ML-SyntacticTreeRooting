bb478e11555f2e1fb951dc9c6d3a735dcd2024fa
AimbotParce <marcparcerisa@gmail.com>
2025-05-02 02:02:37 +0200

    Attempts at graph neural networks

--- src/lib/trees.py
+++ src/lib/trees.py
@@ -1,7 +1,90 @@
 import ast
-from typing import List, Tuple
+from typing import Callable, Dict, List, Tuple
 
 import networkx as nx
+import numpy as np
+import spektral.data
+import torch
+from numpy.typing import NDArray
+from torch_geometric.data import Data
+
+
+def to_spektral(
+    nx_tree: nx.Graph,
+    node_features: Dict[int, NDArray[np.float32]] = None,
+    node_labels: Dict[int, NDArray[np.float32]] = None,
+) -> spektral.data.Graph:
+    """
+    Convert a NetworkX graph to a Spektral graph.
+    Take the opportunity to add node features.
+    """
+    # Get the adjacency matrix
+    A = nx.to_numpy_array(nx_tree)
+
+    node_to_index = {node: i for i, node in enumerate(nx_tree.nodes())}
+
+    if node_features is not None:
+        # Create the node features array
+        node_features_length = len(next(iter(node_features.values())))
+        X = np.zeros((nx_tree.number_of_nodes(), node_features_length))
+        for node in enumerate(nx_tree.nodes()):
+            X[node_to_index[node]] = node_features[node]
+    else:
+        X = np.zeros((nx_tree.number_of_nodes(), 1))
+
+    if node_labels is not None:
+        # Create the node labels array
+        node_labels_length = len(next(iter(node_labels.values())))
+        y = np.zeros((nx_tree.number_of_nodes(), node_labels_length))
+        for node, label in node_labels.items():
+            y[node_to_index[node]] = label
+    else:
+        y = np.zeros((nx_tree.number_of_nodes(), 1))
+
+    # Create the Spektral graph
+    G = spektral.data.Graph(x=X, a=A, y=y, num_nodes=nx_tree.number_of_nodes(), num_edges=nx_tree.number_of_edges())
+    return G
+
+
+def to_torch(
+    nx_tree: nx.Graph,
+    node_features: Dict[int, NDArray[np.float32]] = None,
+    node_labels: Dict[int, NDArray[np.float32]] = None,
+) -> Data:
+    """
+    Convert a NetworkX graph to a PyTorch Geometric graph.
+    Take the opportunity to add node features.
+    """
+    # Get the adjacency matrix
+    A = nx.to_numpy_array(nx_tree)
+
+    node_to_index = {node: i for i, node in enumerate(nx_tree.nodes())}
+
+    if node_features is not None:
+        # Create the node features array
+        node_features_length = len(next(iter(node_features.values())))
+        X = np.zeros((nx_tree.number_of_nodes(), node_features_length))
+        for node in nx_tree.nodes():
+            X[node_to_index[node]] = node_features[node]
+    else:
+        X = np.zeros((nx_tree.number_of_nodes(), 1))
+
+    if node_labels is not None:
+        # Create the node labels array
+        node_labels_length = len(next(iter(node_labels.values())))
+        y = np.zeros((nx_tree.number_of_nodes(), node_labels_length))
+        for node, label in node_labels.items():
+            y[node_to_index[node]] = label
+    else:
+        y = np.zeros((nx_tree.number_of_nodes(), 1))
+
+    G = Data(
+        x=torch.from_numpy(X).float(),
+        edge_index=torch.from_numpy(np.array(nx.to_numpy_array(nx_tree).nonzero())).long(),
+        edge_attr=None,
+        y=torch.from_numpy(y).float(),
+    )
+    return G
 
 
 def get_tree(edge_list: List[Tuple[int, int]]) -> nx.Graph:
